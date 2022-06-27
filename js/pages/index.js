91dd408fe60a8bd5ad0f7c787fc3f2845aed6d43
Louis Moraes <lmoraes@hotmail.fr>
2022-06-27 13:57:54 +0200

    Feat : App interface in Bootstrap 4

--- js/pages/index.js
+++ js/pages/index.js
@@ -5,10 +5,20 @@ init()
 
 let data
 
+document
+  .querySelectorAll('.dropdown')
+  .forEach((dropdown) =>
+    dropdown.addEventListener('click', (e) => toggleDropdown(e.target))
+  )
+
 async function init() {
   data = await API.getRecipes()
 }
 
 export function getData() {
   return data
+}
+
+function toggleDropdown(el) {
+  el.classList.toggle('expanded')
 }
